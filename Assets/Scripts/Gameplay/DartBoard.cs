50eeffd04f8ac088262014305e6910fa12b823f4
LEE WEI LIANG <140511H@sidm.student.local>
2016-08-29 16:55:12 +0800

    Updated Scene

---
+++ Assets/Scripts/Gameplay/DartBoard.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DartBoard : MonoBehaviour
+{
+
+    #region [ Private Vars ]
+    int _score;
+    #endregion
+
+    #region [ Public Vars ]
+    public int[] scoreAmount;
+    public int segments;
+    #endregion
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Dart")
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+
+            //Calculate score based on angle and position
+            Vector3 direction = other.gameObject.transform.position - gameObject.transform.position;
+            float distance = direction.magnitude;
+
+            //Angle calculation
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            angle += 180.0f;
+
+            int finalAngle = (int)angle / segments;
+            _score = scoreAmount[finalAngle];
+
+            //Distance calculation - Outer Bullseye
+            if(distance < 0.5f)
+            {
+                _score = 50;
+            }
+            //Distance calculation - Inner Bullseye
+            else if(distance >= 0.5f && distance < 1.0f)
+            {
+                _score = 25;
+            }
+            //Distance calculation - Outer Ring
+            else if (distance >= 0.5f && distance < 1.0f)
+            {
+                _score *= 2;
+            }
+            //Distance calculation - Inner Ring
+            else if (distance >= 0.5f && distance < 1.0f)
+            {
+                _score *= 3;
+            }
+        }
+    }
+}
