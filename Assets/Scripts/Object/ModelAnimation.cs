bade4668cf30530b39c5f93387bca1e3845ccfe2
ElIgnis <Intervention.408@live.com>
2016-08-31 02:19:29 +0800

    Set up simple animations Added in animations for side guy

---
+++ Assets/Scripts/Object/ModelAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelAnimation : MonoBehaviour {
+
+    Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(anim.isPlaying == false)
+        {
+            anim.Play();
+        }
+	}
+
+    void OnCollisionEnter()
+    {
+        anim.Play("SideGuy_Hit", PlayMode.StopAll);
+    }
+}
